e4a7002927662c4fb9c72791d5e4ecef1c792f00
Danny Dulai <danny@roonlabs.com>
2019-07-20 19:34:16 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# [Roon](https://roonlabs.com) [Extension](node-roon-api) to provide [volume control](https://github.com/RoonLabs/node-roon-api-volume-control) for [Devialet](https://devialet.com/)'s [Phantom Premier](https://www.devialet.com/en-us/phantom-premier-speaker) via its UPnP implementation.
+
+This extension connects to your Devialet Phantom via UPnP, and allows Roon to control it's volume in-app.
+
+This can be useful for when you have your Phantom hooked up via it's optical or analog inputs.
+
+Make sure to configure your Phantom (via Spark) to turn on UPnP support (even if it says beta).
+
+After running this extension and connecting it to your Zone, it will automatically find your Phantoms.
---
+++ app.js
@@ -0,0 +1,172 @@
+"use strict";
+
+const ssdp = require('node-ssdp').Client,
+      upnp = require('node-upnp'),
+      net  = require('net')
+      ;
+
+const ssdpclient = new ssdp();
+let remotes = {};
+
+function Remote(upnp) {
+//    console.log(upnp);
+    this.getVolume = async () => {
+        return (await upnp.call('RenderingControl', 'GetVolume', {
+                    InstanceID: 0,
+                    Channel: 'Master'
+                })).CurrentVolume;
+    };
+    this.getMute = async () => {
+        return (await upnp.call('RenderingControl', 'GetMute', {
+                    InstanceID: 0,
+                    Channel: 'Master'
+                })).CurrentMute;
+    };
+    this.setVolume = async (v) => {
+        return (await upnp.call('RenderingControl', 'SetVolume', {
+                    InstanceID: 0,
+                    Channel: 'Master',
+                    DesiredVolume: v
+                })).CurrentVolume;
+    };
+    this.setMute = async (v) => {
+        return (await upnp.call('RenderingControl', 'SetMute', {
+                    InstanceID: 0,
+                    Channel: 'Master',
+                    DesiredMute: v ? 1 : 0
+                })).CurrentMute;
+    };
+}
+
+function Discover(cb) {
+    ssdpclient.on('response', async function inResponse(headers, code, rinfo) {
+        if (headers.ST == "urn:schemas-upnp-org:service:RenderingControl:2") {
+//        console.log(arguments);
+            if (remotes[headers.USN]) return;
+
+            let u = new upnp({ url: headers.LOCATION });
+            let remote = remotes[headers.USN] = new Remote(u);
+
+            let desc = await u.getDeviceDescription();
+
+//                console.log("found", desc.UDN, desc.friendlyName,
+//                            desc.manufacturer,
+//                            desc.modelName,
+//                            desc.modelNumber);
+
+            if (desc && desc.manufacturer == "Devialet" && desc.modelName == "Devialet UPnP Renderer") {
+                remote.id = desc.UDN;
+                remote.name = desc.friendlyName;
+                cb(remote);
+            }
+
+            // My ancient Phantom (the chrome one) does not support this, so I'm giving
+            // gup on events coming from Spark/PhantomKnob
+//            await u.subscribe('RenderingControl', function (a) {
+//                console.log('XXX', arguments);
+//            });
+        }
+    })
+
+    function search() {
+        ssdpclient.search('urn:schemas-upnp-org:service:RenderingControl:2');
+        setTimeout(function() { search(); }, 2000)
+    }
+
+    search();
+}
+
+//    console.log("Searching for Devialet Bridge...") 
+
+var RoonApi              = require("node-roon-api"),
+    RoonApiStatus        = require('node-roon-api-status'),
+    RoonApiVolumeControl = require('node-roon-api-volume-control');
+
+var roon = new RoonApi({
+    extension_id:        'com.roonlabs.devialet.phantom.volume',
+    display_name:        'Devialet Phantom Volume Control',
+    display_version:     "1.0.0",
+    publisher:           'Roon Labs, LLC',
+    email:               'contact@roonlabs.com',
+    website:             'https://github.com/RoonLabs/roon-extension-devialet-phantom-volume',
+});
+
+let devices = 0;
+var svc_status = new RoonApiStatus(roon);
+var svc_volume_control = new RoonApiVolumeControl(roon);
+
+roon.init_services({
+    provided_services: [ svc_volume_control, svc_status ]
+});
+
+function setup() {
+    new Discover((r) => {
+        if (r.volume_control) { r.volume_control.destroy(); delete(r.volume_control); }
+//        console.log(r);
+        ev_connected(r);
+    });
+
+    svc_status.set_status("Searching...", false);
+}
+
+async function ev_connected(r) {
+    devices++;
+    svc_status.set_status(`Found ${devices} ${devices == 1 ? "device" : "devices"}`, false);
+
+    console.log(r.id);
+    r.volume_control = svc_volume_control.new_device({
+	state: {
+            control_key:  r.id,
+	    display_name: `${r.name}`,
+	    volume_type:  "number",
+	    volume_min:   1,
+	    volume_max:   100,
+	    volume_value: await r.getVolume(),
+	    volume_step:  1.0,
+	    is_muted:     await r.getMute()
+	},
+	set_volume: async function (req, mode, value) {
+            let volume = await r.getVolume();
+	    let newvol = mode == "absolute" ? value : (volume + value);
+	    if      (newvol < this.state.volume_min) newvol = this.state.volume_min;
+	    else if (newvol > this.state.volume_max) newvol = this.state.volume_max;
+            await r.setVolume(newvol);
+            ev_volume(r, newvol);
+            req.send_complete("Success");
+	},
+	set_mute: async function (req, action) {
+            let mute = action == 'on';
+            if (action == 'toggle')
+                mute = !(await r.getMute());
+            r.setMute(mute);
+            ev_mute(r, mute);
+	    req.send_complete("Success");
+	}
+    });
+}
+
+function ev_disconnected(r) {
+    devices--;
+    if (devices == 0) {
+        svc_status.set_status("Searching...", false);
+    } else {
+        svc_status.set_status(`Found ${devices} ${devices == 1 ? "device" : "devices"}`, false);
+    }
+
+    if (r.volume_control) { r.volume_control.destroy(); delete(r.volume_control);   }
+}
+
+function ev_volume(r, val) {
+    console.log("[Devialet Phantom Extension] received volume change from device:", val);
+    if (r.volume_control)
+        r.volume_control.update_state({ volume_value: val });
+}
+function ev_mute(r, val) {
+    console.log("[Devialet Phantom Extension] received volume change from device:", val);
+    if (r.volume_control)
+        r.volume_control.update_state({ is_muted: val });
+}
+
+setup();
+
+roon.start_discovery();
